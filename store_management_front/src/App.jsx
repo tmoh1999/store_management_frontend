29acf1235338fa624ad81c590fd3b6730c946fca
* <mohamedtouabti1999@gmail.com>
2025-11-15 17:29:10 +0100

    removed files from import , add in future

--- store_management_front/src/App.jsx
+++ store_management_front/src/App.jsx
@@ -1,16 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import ManageProducts from "./pages/ManageProducts";
-import ManageSales from "./pages/ManageSales";
 // you will create these pages
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/products/manageproducts" element={<ManageProducts />} />
-      <Route path="/sales/managesales" element={<ManageSales />} />
-      {/* Add the remaining routes */}
     </Routes>
   );
 }
