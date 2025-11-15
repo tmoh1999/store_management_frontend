d44f4af43d3fae22fdad1fec422fa58bc013120d
* <mohamedtouabti1999@gmail.com>
2025-11-15 16:49:49 +0100

    changes version of tailwind to 3.3 to avoid lightning css on termux

--- store_management_front/tailwind.config.cjs
+++ store_management_front/tailwind.config.cjs
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  experimental : {
-  compile: 'swc' ,
-  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
