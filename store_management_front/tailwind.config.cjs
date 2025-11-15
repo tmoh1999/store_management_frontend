57f9f870500756eae65744e507f0462b5c1f98d3
* <mohamedtouabti1999@gmail.com>
2025-11-15 16:42:58 +0100

    changes complie to js in tailwind

--- store_management_front/tailwind.config.cjs
+++ store_management_front/tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  experimental : {
+  compile: 'swc' ,
+  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
