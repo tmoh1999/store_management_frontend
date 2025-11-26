bb91167dc8ec96b96c8defaa2fc3df0eb2a9b508
* <mohamedtouabti1999@gmail.com>
2025-11-26 11:49:59 +0100

    added simple salescreen

---
+++ store_management_front/src/pages/SaleScreen.jsx
@@ -0,0 +1,89 @@
+import {addSaleItem,getSaleItems} from "../api"
+import Table from "../Table";
+import { useEffect ,useState} from "react";
+import TableCell from "../TableCell"
+export default function SaleScreen({sale_id}){
+const [formData, setFormData] = useState({
+    description: "",
+    quantity: 1,
+    price:0.0,
+  });
+const [reload,setReload]=useState(false);
+ const [saleItems,setSaleItems]=useState({
+    columns: [],
+    data: []
+  });
+const handleChange = (e) => {
+	console.log(e.target.value);
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+const handleClick = async () => {
+   try {
+   	console.log(formData);
+       const result=await addSaleItem(formData,sale_id);
+       console.log(result.results);
+       setReload(prev => !prev);
+   }catch(err){
+       console.log(err);
+   }
+};
+useEffect(() => {
+    getSaleItems(sale_id)
+   .then(result => {
+        console.log(result.results);
+        
+        setSaleItems(prev => ({
+  ...prev,
+  columns: [
+    { label: "ID", accessor: "id" ,edit:false},
+    { label: "Barcode", accessor: "barcode" ,edit:false },
+    { label: "Name", accessor: "name" ,edit:false },
+    { label: "Unit Price", accessor: "price" ,edit:true },
+    { label: "Quantity", accessor: "quantity" ,edit:true },
+    { label: "Description", accessor: "description" ,edit:true },
+  ],
+  data: result.results
+}));
+    });
+}, [reload]);
+return (
+<div >
+
+<table className="w-auto shadow-md">
+<thead >
+  <tr className="bg-gray-200">
+                    <th className="p-3 cursor-pointer border">Name</th>
+                    <th className="p-3 cursor-pointer border">Barcode</th>
+                    <th className="p-3 cursor-pointer border">Description</th>
+                    <th className="p-3 cursor-pointer border">Price</th>
+                    <th className="p-3 cursor-pointer border">Quantity</th>
+  </tr>
+</thead>
+
+<tbody>
+     <tr className="bg-gray-100">
+                    <TableCell Editable={false} val=""/>
+                    <TableCell Editable={false} val=""/>
+                    <TableCell Editable={true} val={formData.description} type="text" name="description" onChanged={handleChange}/>
+                    <TableCell Editable={true} val={formData.price} type="number" name="price" onChanged={handleChange}/>
+                    <TableCell Editable={true} val={formData.quantity} type="number" name="quantity" onChanged={handleChange}/>
+    </tr>
+</tbody>
+</table>
+<div className="mt-4 mr-4 flex justify-end">
+<button className="mb-3 p-1 text-xl text-white font-medium shadow-lg rounded-xl bg-blue-500 hover:bg-blue-600 "
+onClick={handleClick}
+>➕ Add Item</button>
+</div>
+<Table data={saleItems.data} columns={saleItems.columns}  rootpath="/api/sales" 
+    refreshParent={() =>{
+    	setReload(prev => !prev);
+    }}/>
+</div>
+
+)
+}
--- store_management_front/src/pages/Sales.jsx
+++ store_management_front/src/pages/Sales.jsx
@@ -1,7 +1,34 @@
 import { Link ,useNavigate,useLocation} from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
+import {addSale} from "../api"
+import SaleScreen from "./SaleScreen"
 export default function Sales() {
+const [sale_id,setSaleId]=useState(0)
+const [openSaleScreen,setOpenSaleScreen]=useState(false)
+ const handleClick= async () => {
+     try{
+         const result = await addSale()
+         
+         setSaleId(result.sale_id);
+         setOpenSaleScreen(true);
+         
+     }catch(err){
+          console.log(err);
+     }
+ };
   return (
-<div><h1>Sales</h1></div>
+<div className="flex flex-col ">
+<h1 className="p-1 text-3xl text-center font-bold ">Sales</h1>
+<button className="self-start p-3 mb-3 text-2xl bg-green-500 shadow-lg rounded-xl hover:bg-green-700 text-white  font-medium"
+onClick={handleClick}
+>
+Start Sale
+</button>
+{sale_id!==0 &&
+<SaleScreen sale_id={sale_id}/>
+}
+
+</div>
+
   );
 }
