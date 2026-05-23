c18d0d0573b458b3a68ed6889cead454e9f4a0fb
mohamed <mohamedtouabti1999@gmail.com>
2026-05-23 18:11:50 +0100

    fix: catch errors+clean

--- store_management_front/src/pages/TransactionScreen.jsx
+++ store_management_front/src/pages/TransactionScreen.jsx
@@ -1,45 +1,40 @@
 import {apiGet,addSaleItem,updateSaleItem,removeRow,confirmSale,addPurchaseItem,updatePurchaseItem,confirmPurchase, searchProduct} from "../api"
-import Table from "../Table";
 import { useEffect ,useState} from "react";
 import TableCell from "../TableCell"
 import DataTable from "../DataTable";
 import BarcodeScanner from "../BarcodeScanner";
 export default function TransactionScreen({mode,transaction_id,setTransactionId}){
-const tablename= mode=="sale" ? "Sale N°"+transaction_id.toString()+" Items" : "Purchase N°"+transaction_id.toString()+" Items"
-const [product,setProduct]=useState(null);
 const [openSelectMenu,setOpenSelectMenu]=useState(false);
 const [showScanner, setShowScanner] = useState(false);
+const [error, setError] = useState("");
 const [total,setTotal]=useState(0);
 const [refreshKey,setRefreshKey]=useState(0);
-const [formData, setFormData] = mode=="sale" ? 
-useState({
-    product_id:null,
-    barcode:"",
-    name:"",
-    description: "",
-    quantity: 1,
-    price:0.0,
-    discount:0.0,
-    discount_type:"percentage",
-  })
-  :
-useState({
-    product_id:null,
-    barcode:"",
-    name:"", 
-    latest_purchase_price:"??", 
-    quantity: 1,
-    price:0.0,
-  })
-  ;
-   
+const [formData, setFormData] = useState(
+    mode === "sale" ? {
+        product_id: null,
+        barcode: "",
+        name: "",
+        description: "",
+        quantity: 1,
+        price: 0.0,
+        discount: 0.0,
+        discount_type: "percentage",
+    } : {
+        product_id: null,
+        barcode: "",
+        name: "",
+        latest_purchase_price: "??",
+        quantity: 1,
+        price: 0.0,
+    }
+); 
 // 1️⃣ API MAPPING BASED ON MODE
 const api = {
   sale: {
     addItem: addSaleItem,
     datamod:"sale_items",
     options:{sale_id:transaction_id},
-    tablename:`Sale Items , Sale_id:${transaction_id}`,
+    tablename:"Sale Items",
     confirm: confirmSale,
     remove:removeRow
   },
@@ -47,14 +42,13 @@ const api = {
     addItem: addPurchaseItem,
     datamod:"purchase_items",
     options:{purchase_id:transaction_id},
-    tablename:`Purchase Items , Purchase_id:${transaction_id}`,
+    tablename:"Purchase Items",
     confirm: confirmPurchase,
     remove:removeRow
   }
 }[mode];
 
 const handleChange = (e) => {
-	console.log(e.target.value);
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
@@ -63,11 +57,13 @@ const handleChange = (e) => {
   };
 const handleClick = async () => {
    try {
-   	console.log(formData);
        const result=await api.addItem(formData,transaction_id,formData.product_id);
-       console.log("sss"+result.success);
+       if(!result?.success){
+          setError(result?.message  || `add ${mode} item failed`);
+          return;
+       }
    }catch(err){
-       console.log(err);
+      setError(err?.message  || `add ${mode} item failed`);
    }finally{
       setRefreshKey(prev=> prev+1);
    }
@@ -75,34 +71,38 @@ const handleClick = async () => {
 
 const CancelClick = async () => {
    try {
-   	const path=mode=="sale" ? "/api/sales/"+transaction_id+"/remove" : "/api/purchases/"+transaction_id+"/remove";
+   	const path=mode==="sale" ? "/api/sales/"+transaction_id+"/remove" : "/api/purchases/"+transaction_id+"/remove";
        const result=await api.remove(path);
-       console.log(result.results);
+       if(!result?.success){
+          setError(result?.message  || `cancel ${mode} failed`);
+          return;
+       }       
        setTransactionId(0);
        
    }catch(err){
-       console.log(err);
+       setError(err?.message  || `cancel ${mode} failed`);
    }
 };
 
 const ConfirmClick = async () => {
-   try {
-   	
-       const result=await api.confirm(transaction_id);
-       console.log(result);
-       setTransactionId(0);
-       
-   }catch(err){
-       console.log(err);
-   }
+  try {
+
+    const result=await api.confirm(transaction_id);
+    if(!result?.success){
+      setError(result?.message  || `confirm ${mode} failed`);
+      return;
+    }         
+
+    setTransactionId(0);
+      
+  }catch(err){
+    setError(err?.message  || `confirm ${mode} failed`);
+  }
 };
 
 
 
 const changeProduct=(row)=>{
-  console.log("ssss");
-  console.log(row);
-  console.log("ssss");
   setFormData(prev => ({
     ...prev,
     product_id:row.id,
@@ -113,43 +113,51 @@ const changeProduct=(row)=>{
   setOpenSelectMenu(false);
 };
 useEffect (()=>{
-  if (mode=="purchase"){
+  if (mode==="purchase"){
     apiGet("/api/products/latest_purchase_price",{product_id:formData.product_id})
     .then(result => {
-      console.log(result);
+      if(!result?.success){
+        setError(result?.message  || "loading latest_purchase_price data failed.");
+        return;
+      }         
       setFormData(prev =>({
         ...prev,
         latest_purchase_price:(result.success && result.purchase_price>0)? String(result.purchase_price) : "??",
       }));
-
     })
+    .catch(err=>{
+      setError(err?.message  || "loading latest_purchase_price data failed.");
+    });
   }
 },[formData.product_id]);
 
-const startScanning = () => {
-  setShowScanner(true);
-};
+
 const onDetected = async (code)=> {
-  console.log("code:::"+code);
   
   setShowScanner(false);
   try{
     const result=await searchProduct("barcode",code)
-    if(result.success){
-      console.log(result);
+    if(result?.success){
       setFormData(prev => ({
         ...prev,
         barcode:result.product_barcode,
         name:result.product_name,
         product_id:result.product_id,
       }));
+    }else{
+      setError(result?.message  || `scanning barcode failed.`);
+      return;
     }
   }catch(error){
-    console.log(error.message);
+    setError(error?.message  || `scanning barcode failed.`);
   }
 };
 return (
 <div>
+  {/* Error Box */}
+  {error && (
+    <div className="bg-red-100 text-red-700 p-2 rounded">{error}</div>
+  )}  
   {openSelectMenu ? (
   <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
     <div className="h-screen overflow-y-auto shadow-lg rounded-lg bg-white p-2">
@@ -160,14 +168,14 @@ return (
   <>
   <div className="mt-2 flex flex-col items-center justify-center ">
     <div className="w-fit">
-      <h1 className="p-1 text-3xl text-center font-bold ">{mode=="purchase" ? "Purchase:": "Sale:"}{transaction_id}    Total:{total}</h1>
+      <h1 className="p-1 text-3xl text-center font-bold ">{mode==="purchase" ? "Purchase:": "Sale:"}{transaction_id}    Total:{total}</h1>
       <table className="w-auto shadow-md">
       <thead >
         <tr className="bg-gray-200">
                           <th></th>
                           <th className="p-3 cursor-pointer border">Name</th>
                           <th className="p-3 cursor-pointer border">Barcode</th>
-                          {mode=="purchase" &&
+                          {mode==="purchase" &&
                             (
                               <>
 
@@ -178,7 +186,7 @@ return (
                           }  
                           <th className="p-3 cursor-pointer border">Price</th>
                           <th className="p-3 cursor-pointer border">Quantity</th>
-                          {mode=="sale" &&
+                          {mode==="sale" &&
                             (
                               <>
 
@@ -198,14 +206,14 @@ return (
       <tbody>
           <tr className="bg-gray-100">
                           <td>            
-                            <button onClick={startScanning} className="bg-green-600 text-white px-3 rounded-lg hover:bg-green-700 ml-2">
+                            <button type="button" onClick={() => setShowScanner(true)} className="bg-green-600 text-white px-3 rounded-lg hover:bg-green-700 ml-2">
                             Scan
                             </button>
                           </td>
                           <TableCell Editable={false} val={formData.name}/>
                           <TableCell Editable={false} val={formData.barcode}/>
                           {
-                            mode=="purchase" && (
+                            mode==="purchase" && (
                               <>
                                 <TableCell Editable={false} val={formData.latest_purchase_price}/>
                               </>
@@ -213,7 +221,7 @@ return (
                           }
                           <TableCell Editable={true} val={formData.price} type="number" name="price" onChanged={handleChange}/>
                           <TableCell Editable={true} val={formData.quantity} type="number" name="quantity" onChanged={handleChange}/>
-                          {mode=="sale" &&
+                          {mode==="sale" &&
                             (
                               <>
                               <TableCell Editable={true} val={formData.discount} type="number" name="discount" onChanged={handleChange}/>
@@ -239,21 +247,21 @@ return (
 
       <div className=" mt-2 flex justify-between ">
         <button className="mb-3 p-1 text-xl text-white font-medium shadow-lg rounded-xl bg-green-500 hover:bg-green-600 "
-        onClick={()=>{setOpenSelectMenu(true);}}
+        type="button" onClick={()=>{setOpenSelectMenu(true);}}
         >select product</button>
         <button className="mb-3 p-1 text-xl text-white font-medium shadow-lg rounded-xl bg-blue-500 hover:bg-blue-600 "
-        onClick={handleClick}
+        type="button" onClick={handleClick}
         >➕ Add Item</button>
       </div>
     </div>
 
     <div className="mt-8">
       <button className="mr-10 mb-3 p-1 text-xl text-white font-medium shadow-lg rounded-xl bg-green-500 hover:bg-green-600 "
-      onClick={ConfirmClick}
+      type="button" onClick={ConfirmClick}
       >💾 Confirm</button>
 
       <button className="ml-10 mb-3 p-1 text-xl text-white font-medium shadow-lg rounded-xl bg-red-500 hover:bg-red-600 "
-      onClick={CancelClick}
+      type="button" onClick={CancelClick}
       >❌ Cancel</button>
     </div>
     <div className="mt-2">
@@ -278,7 +286,7 @@ return (
     <BarcodeScanner onDetected={onDetected} />
         <button 
             className="mt-3 bg-red-500 text-white p-2 rounded-lg w-full hover:bg-red-600"
-            onClick={() => setShowScanner(false)}
+            type="button" onClick={() => setShowScanner(false)}
           >
             Close
         </button>
