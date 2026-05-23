0c75f43f985c3e17a0f0eacf330078dfe7b7fea4
mohamed <mohamedtouabti1999@gmail.com>
2026-05-23 19:46:41 +0100

    clean: removed console log

--- store_management_front/src/ProtectedRoute.jsx
+++ store_management_front/src/ProtectedRoute.jsx
@@ -3,6 +3,5 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
-  console.log("token"+token);
   return token ? children : <Navigate to="/login" />;
 }
