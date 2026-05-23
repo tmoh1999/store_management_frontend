db0a89301574aa524ec6570db7d70639ecae0b8f
mohamed <mohamedtouabti1999@gmail.com>
2026-05-23 10:49:19 +0100

    fix(Register):  fix+clean page

--- store_management_front/src/pages/Register.jsx
+++ store_management_front/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { Link , useNavigate} from "react-router-dom";
-import { useEffect ,useState} from "react";
+import { useState} from "react";
 import {register} from "../api";
 export default function Register() {
 // Step 1: Create state for form fields
@@ -21,55 +21,46 @@ const navigate = useNavigate();
 
   // Step 3: Handle form submission
   const handleSubmit = async (e) => {
-  	
     e.preventDefault(); // Prevent page reload
-    console.log(formData["username"]+"...."+formData["password"] );
     setError("");
     setLoading(true);
     try {
-  const result = await register(formData);
+      const result = await register(formData);
 
-  // Save token
-  localStorage.setItem("token", result.token);
-  console.log(result.token)
-  console.log(result.user)
-  // Redirect (optional)
-  const user={
-        username:result.user.username,
-  };
-   navigate("/dashboard",{
-   state:user,
-   });
-} catch (err) {
-  setError(err.message || "Login failed");
-} finally {
+      // Save token
+      localStorage.setItem("token", result.token);
+      localStorage.setItem("username", result.user.username);
+
+      navigate("/dashboard");
+    } catch (err) {
+      setError(err.message || "Registration  failed");
+    } finally {
       setLoading(false);
     }
     // Here you can call an API or do further processing
   };
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-    
-    
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow-lg w-80 flex flex-col items-center gap-4">
-<h1 className="text-2xl font-bold text-center">Login</h1>
-  {/* Error Box */}
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+        {/* Error Box */}
         {error && (
           <div className="bg-red-100 text-red-700 p-2 rounded">{error}</div>
-        )}
-        
-        
-  <input onChange={handleChange} value={formData.username} name="username" className="border p-2 rounded-lg w-full"  required/>
-  <input onChange={handleChange} value={formData.password} type="password" className="border p-2 rounded-lg w-full"   name="password" required/>
+        )}   
+        <label htmlFor="username">Username</label>      
+        <input onChange={handleChange} value={formData.username} id="username" name="username" className="border p-2 rounded-lg w-full"  required/>
+        <label htmlFor="password">Password</label>          
+        <input onChange={handleChange} value={formData.password} id="password" name="password" type="password" className="border p-2 rounded-lg w-full" required/>
 
-  <button type="submit" disabled={loading} className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-700">{loading ? "Registering..." : "Register"}</button>
-  
-
-  
-
-      
+        <button type="submit" disabled={loading} className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-700">{loading ? "Registering..." : "Register"}</button>      
+        <p>Already have an account?
+          <Link 
+            to="/login" 
+            className="ml-2 text-green-600 font-medium hover:underline">
+            Login
+          </Link>
+        </p>
       </form>
-      
     </div>
   );
 } 
