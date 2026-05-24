8cf2fd9f877872c500a52e5c23058e756af03f9f
mohamed <mohamedtouabti1999@gmail.com>
2026-05-24 19:10:02 +0100

    fix: fix+clean

--- store_management_front/src/pages/Purchases.jsx
+++ store_management_front/src/pages/Purchases.jsx
@@ -1,7 +1,5 @@
-import { useEffect ,useState} from "react";
-import {removeRow, savePurchaseRow} from "../api"
+import { useEffect ,useState , useRef} from "react";
 import TransactionScreen from "./TransactionScreen"
-import Table from "../Table";
 import StartPurchase from "./StartPurchase";
 import DataTable from "../DataTable";
 import {useLocation} from "react-router-dom";
@@ -9,11 +7,8 @@ export default function Purchases() {
   const {state}=useLocation();
 const [purchase_id,setPurchaseId]=useState(0)
 const [openStartPurchase,setOpenStartPurchase]=useState(false);
-const [reload,setReload]=useState(false);
-const handleClick=  () => {
-  setOpenStartPurchase(true);
-};
-
+const [refreshKey,setRefreshKey]=useState(0);
+const isFirstRender = useRef(true);
 useEffect(() => {
   if(state?.id){
     setPurchaseId(state.id);
@@ -22,8 +17,12 @@ useEffect(() => {
 
 
 useEffect(() => {
-  if (purchase_id==0){
-    setReload(prev => !prev);
+  if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+  }
+  if (purchase_id===0){
+    setRefreshKey(prev => prev + 1);
   }
 },[purchase_id]);
  
@@ -35,18 +34,19 @@ useEffect(() => {
     <StartPurchase setPurchaseID={setPurchaseId} setOpenStartPurchase={setOpenStartPurchase}/>
   ):(
     <div className=" p-2 w-fit mt-3">
-      {purchase_id==0 ? (
+      {purchase_id===0 ? (
       <>
         <div className="flex justify-end">
-          <button onClick={handleClick} className="self-start p-3 mb-2 text-2xl bg-green-500 shadow-lg rounded-xl hover:bg-green-700 text-white  font-medium">
+          <button type="button" onClick={() => setOpenStartPurchase(true)} className="self-start p-3 mb-2 text-2xl bg-green-500 shadow-lg rounded-xl hover:bg-green-700 text-white  font-medium">
           Start Purchase
           </button> 
         </div> 
         <DataTable
             mode="purchases"
             getOptions={{}}
-            TableName={`Purchases`}
+            TableName="Purchases"
             Edit={true}
+            refreshKey={refreshKey}
         />  
       </>
 
